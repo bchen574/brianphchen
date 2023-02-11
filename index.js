a4e63eb8b8ba361cd58bfbddbf0530d33cb6a4c0
bchen574 <124513037+bchen574@users.noreply.github.com>
2023-02-10 20:45:34 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-window.addEventListener('DOMContentLoaded', () => {
-
-	const observer = new IntersectionObserver(entries => {
-	  entries.forEach(entry => { const id = entry.target.getAttribute('id');
-		if (entry.isIntersecting) {
-		  document.querySelector(`[href="#${id}"]`).classList.add('active');
-		} else {
-		  document.querySelector(`[href="#${id}"]`).classList.remove('active');
-		}
-	  });
-	});
-  
-	// Track all sections that have an `id` applied
-	document.querySelectorAll('section[id]').forEach((section) => {
-	  observer.observe(section);
-	});
-	
+window.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("id");
+      if (entry.isIntersecting) {
+        document.querySelector(`[href="#${id}"]`).classList.add("active");
+      } else {
+        document.querySelector(`[href="#${id}"]`).classList.remove("active");
+      }
+    });
   });
 
-  
-  var prevScrollpos = window.pageYOffset;
-  window.onscroll = function() {
-	var currentScrollPos = window.pageYOffset;
-	if (prevScrollpos > currentScrollPos) {
-	  document.getElementById("navbar").style.top = "0";
-	} else {
-	  document.getElementById("navbar").style.top = "-70px";
-	}
-	prevScrollpos = currentScrollPos;
-  }
+  // Track all sections that have an `id` applied
+  document.querySelectorAll("section[id]").forEach((section) => {
+    observer.observe(section);
+  });
+});
 
- 
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-45px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+const toggleButton = document.getElementsByClassName('hamburger')[0]
+const navbarLinks = document.getElementsByClassName('mobile_navbarlinkscontainer')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
